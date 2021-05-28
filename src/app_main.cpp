8eac43bf18260a0a96479bba37f0c5f92c74162c
Ashot Madatyan <ashotm71@gmail.com>
2021-05-28 13:48:41 +0400

    - Initial commit of the object_pool project;

---
+++ src/app_main.cpp
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include "object_pool.h"
+
+using namespace std;
+
+struct no_ctor
+{
+private:
+    no_ctor() {}
+    ~no_ctor() {}
+
+public:
+    int v;
+    string s;
+};
+
+struct cstm_ctbl
+{
+    int v;
+    string s;
+
+    cstm_ctbl(int vv, const string& str) : v(vv), s(str) {}
+};
+
+struct dflt_ctbl
+{
+    int v;
+    string s;
+
+    dflt_ctbl() : v(0) {}
+    ~dflt_ctbl() {}
+};
+
+
+struct test_struct
+{
+    int val;
+    string str;
+
+    void print()
+    {
+        printf("val: %d str: %s\n", val, str.c_str());
+    }
+
+    test_struct() : val(0)
+    {
+        static int counter = 1;
+        str = to_string(counter);
+        ++counter;
+    }
+
+    test_struct(const int& val, const string& s) : val(val), str(s)
+    {
+        static int counter = 1;
+        //str = to_string(counter);
+        ++counter;
+    }
+};
+
+
+void test_object_pool()
+{
+    object_pool<test_struct> gm;
+    vector<std::unique_ptr<test_struct, object_pool<test_struct>::deleter_type>> vvv;
+    using ptr_type = std::unique_ptr<test_struct, object_pool<test_struct>::deleter_type>;
+
+    gm.add(5, "ashot_madatyan");
+
+    for (int i = 0; i < 20; ++i)
+    {
+        ptr_type ptr = gm.get();
+        //printf("got object: %d %s\n", ptr->val, ptr->str.c_str());
+        vvv.push_back(move(ptr));
+    }
+
+    for (int i = 0; i < 4; ++i)
+        vvv.pop_back();
+
+}
+
+int main()
+{
+    test_object_pool();
+    return 0;
+    
+    object_pool<dflt_ctbl> op;
+    object_pool<cstm_ctbl> op1(16, "ashot_madatyan");
+
+    
+    return 0;
+}
+
---
+++ src/object_pool.h
@@ -0,0 +1,131 @@
+#ifndef OBJECT_POOL_HEADER_
+#define OBJECT_POOL_HEADER_
+
+#include <stdio.h>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <memory>
+#include <mutex>
+
+using namespace std;
+
+template <typename T, int S = 10>
+class object_pool
+{
+    // Wrapper for the actual data stored in the object pool
+    struct pool_element
+    {
+        int healty = 1;
+        T* entry;
+
+        pool_element() : entry(nullptr) {}
+        ~pool_element() { if (entry) delete entry; }
+    };
+
+    struct custom_deleter
+    {
+        object_pool* pool_;
+        custom_deleter(object_pool* pool) : pool_(pool) {}
+        void operator()(T* pobject)
+        {
+            if (pobject)
+                pool_->reclaim_object(pobject);
+        }
+    };
+
+    using storage_type = unordered_map<T*, pool_element*>;
+    using custom_deleter_type = typename object_pool::custom_deleter;
+
+    custom_deleter_type object_deleter;
+    storage_type        used_items_;
+    storage_type        free_items_;
+    mutex               mtx_;
+
+public:
+
+    using deleter_type = typename object_pool::custom_deleter;
+    using uptr_type = std::unique_ptr<T, custom_deleter>;
+
+    template<class ...Args>
+    object_pool(Args&& ...args) : object_deleter(this)
+    {
+        for (int i = 0; i < S; ++i)
+        {
+            T* obj = new T{std::forward<Args>(args)...};
+            pool_element* pel = new pool_element;
+            pel->entry = obj;
+            free_items_[obj] = pel;
+        }
+    }
+
+    template<class ...Args>
+    void add(Args&& ...args)
+    {
+        T* pobj = new T(std::forward<Args>(args)...);
+        pool_element* pel = new pool_element;
+        pel->entry = pobj;
+        {
+            lock_guard<mutex> lck(mtx_);
+            free_items_[pobj] = pel;
+        }
+    }
+
+    void reclaim_object(T* pobject)
+    {
+        if (nullptr != pobject)
+        {
+            lock_guard<mutex> lck(mtx_);
+            //pobject->print();
+            storage_type::iterator it = used_items_.find(pobject);
+            if (it == used_items_.end())
+                throw std::runtime_error("object not in the used items store");
+            pool_element* pel = it->second;
+            used_items_.erase(it);
+            free_items_[pobject] = pel;
+        }
+        else
+        {
+            printf("object not found\n");
+        }
+    }
+
+    size_t size() const
+    {
+        lock_guard<mutex> lck(mtx_);
+        return used_items_.size();
+    }
+
+    size_t capacity() const
+    {
+        lock_guard<mutex> lck(mtx_);
+        return used_items_.size() + free_items_.size();
+    }
+
+    uptr_type get()
+    {
+        pool_element* pel(nullptr);
+        
+        if (0 == free_items_.size())
+        {
+            // Alloc new object and return it
+            pel = new pool_element;
+            pel->entry = new T();
+            {
+                lock_guard<mutex> lck(mtx_);
+                used_items_[pel->entry] = pel;
+            }
+            return std::unique_ptr<T, custom_deleter>(pel->entry, object_deleter);
+        }        
+        
+        {
+            lock_guard<mutex> lck(mtx_);
+            pel = free_items_.begin()->second;
+            free_items_.erase(free_items_.begin());
+            used_items_[pel->entry] = pel;
+        }
+        return std::unique_ptr<T, custom_deleter>(pel->entry, object_deleter);
+    }
+};
+
+#endif // OBJECT_POOL_HEADER_
